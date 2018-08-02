6e8b705d13c1d5e596d6db1d2115a270d169f14e
wall-e <zz@admin.com>
2018-08-02 11:11:24 +0800

    增加定时任务，更新网站数据

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'app',
+    'django_crontab',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
+
+CRONJOBS = [
+    ('*/1 * * * *', 'app.cron.load_all.loads')
+]
